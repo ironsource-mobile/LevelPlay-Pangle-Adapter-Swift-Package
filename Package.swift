0e2a1432c27638e90a8075891bb9308b637f0935
Gal Yedidovich <gal.yedidovich@unity3d.com>
2026-01-18 20:43:29 +0200

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+let package = Package(
+  name: "LevelPlay-Pangle-Adapter-Swift-Package",
+  platforms: [.iOS(.v12)],
+  products: [
+    .library(name: "PangleAdapter", targets: ["PangleAdapter"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/bytedance/AdsGlobalPackage", exact: "7.8.5.6"),
+    .package(url: "https://github.com/ironsource-mobile/Unity-Mediation-iAds-Swift-Package", "9.2.0"..<"10.0.0"),
+  ],
+  targets: [
+    .target(
+      name: "PangleAdapter",
+      dependencies: [
+        "PangleAdapterSDK",
+        .product(name: "AdsGlobalPackage", package: "AdsGlobalPackage"),
+        .product(name: "UnityMediationSDK", package: "Unity-Mediation-iAds-Swift-Package"),
+      ]
+    ),
+    .binaryTarget(
+      name: "PangleAdapterSDK",
+      url: "https://raw.githubusercontent.com/ironsource-mobile/iOS-adapters/master/pangle-adapter/5.11.0/ISPangleAdapter5.11.0.zip",
+      checksum: "09510be1dd389ed307bc878184d3f8cac0fb3b3c14fa8f5cd5258dee1ee57006"
+    )
+  ]
+)
