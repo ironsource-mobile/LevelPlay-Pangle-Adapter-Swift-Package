15458d8d2021270d000a8ab43fd7a1882044921d
UnityLevelPlay <svc-levelplay-release@unity3d.com>
2026-04-19 12:10:55 +0000

    Release 5.20.0 - SDK 8.0.0.6

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     .library(name: "PangleAdapter", targets: ["PangleAdapter"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/bytedance/AdsGlobalPackage", exact: "7.9.1.1"),
+    .package(url: "https://github.com/bytedance/AdsGlobalPackage", exact: "8.0.0.6"),
     .package(url: "https://github.com/ironsource-mobile/Unity-Mediation-iAds-Swift-Package", "9.2.0"..<"10.0.0"),
   ],
   targets: [
@@ -22,8 +22,8 @@ let package = Package(
     ),
     .binaryTarget(
       name: "PangleAdapterSDK",
-      url: "https://raw.githubusercontent.com/ironsource-mobile/iOS-adapters/master/pangle-adapter/5.19.0/ISPangleAdapter5.19.0.zip",
-      checksum: "a4585b80832f99d4b2c493193f4962db54b5b3f6e27d7ade68a741c72e38d797"
+      url: "https://raw.githubusercontent.com/ironsource-mobile/iOS-adapters/master/pangle-adapter/5.20.0/ISPangleAdapter5.20.0.zip",
+      checksum: "53eccacb1296e114e18de9e920736e75dc1710e2a26761227aab802ce6801d9e"
     )
   ]
 )
